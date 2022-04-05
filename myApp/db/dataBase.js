2daa35f83c28909d728d103313258505be31854d
francomainardi <fmainardi@udesa.edu.ar>
2022-04-05 19:41:44 -0300

    agregue zapatillas

--- myApp/db/dataBase.js
+++ myApp/db/dataBase.js
@@ -12,52 +12,82 @@ const dataBase = {
     products: [
         {
         model:'Jordan 1 high',
-        description:'Zapatillas de alta calidad. Par indispensable e imperdible, únicas en el país, no perderse esta oportunidad',
+        description:'The Air Jordan 1 Retro High OG Light Smoke Grey emerges with a neutral look and a vibrant accent. The shoes upper features a white leather base overlaid by Light Smoke Grey suede, with black contrast on the Swoosh, collar, laces and tongue tag. The collar flap highlights the look in Varsity Red, matched by the tongue branding, while an Air midsole and concentric outsole support the fit underfoot.',
         image: '/images/products/jordan1.png'
         },
          {
         model:'Jordan 1 high',
-        description:'Zapatillas de alta calidad. Par indispensable e imperdible, únicas en el país, no perderse esta oportunidad',
+        description:'The Air Jordan 1 Retro High OG Light Smoke Grey emerges with a neutral look and a vibrant accent. The shoes upper features a white leather base overlaid by Light Smoke Grey suede, with black contrast on the Swoosh, collar, laces and tongue tag. The collar flap highlights the look in Varsity Red, matched by the tongue branding, while an Air midsole and concentric outsole support the fit underfoot.',
         image: '/images/products/jordan1.png'
         },
-         {
+        {
         model:'Jordan 1 high',
-        description:'Zapatillas de alta calidad. Par indispensable e imperdible, únicas en el país, no perderse esta oportunidad',
+        description:'The Air Jordan 1 Retro High OG Light Smoke Grey emerges with a neutral look and a vibrant accent. The shoes upper features a white leather base overlaid by Light Smoke Grey suede, with black contrast on the Swoosh, collar, laces and tongue tag. The collar flap highlights the look in Varsity Red, matched by the tongue branding, while an Air midsole and concentric outsole support the fit underfoot.',
+        image: '/images/products/jordan1.png'
+        },
+        {
+        model:'Jordan 1 high',
+        description:'The Air Jordan 1 Retro High OG Light Smoke Grey emerges with a neutral look and a vibrant accent. The shoes upper features a white leather base overlaid by Light Smoke Grey suede, with black contrast on the Swoosh, collar, laces and tongue tag. The collar flap highlights the look in Varsity Red, matched by the tongue branding, while an Air midsole and concentric outsole support the fit underfoot.',
         image: '/images/products/jordan1.png'
         },
          {
+        model:'Off White Converse',
+        description:'Virgil Abloh emphasizes accessibility and wearability with the OFF-WHITE x Chuck 70 White collab. As a result, the classic silhouette is ensconced in traditional cotton canvas and finished in a versatile crisp white shade. Virgil retains the shoes signature and rubber license plate, though the latter is turned upside down. Additional flourishes include the OFF-WHITEs trademark diagonal midsole stripes, printed text on the medial side, and a bright orange outsole.',
+        image: '/images/products/offWhiteConverse.webp'
+        },
+         {
+        model:'Off White Converse',
+        description:'Virgil Abloh emphasizes accessibility and wearability with the OFF-WHITE x Chuck 70 White collab. As a result, the classic silhouette is ensconced in traditional cotton canvas and finished in a versatile crisp white shade. Virgil retains the shoes signature and rubber license plate, though the latter is turned upside down. Additional flourishes include the OFF-WHITEs trademark diagonal midsole stripes, printed text on the medial side, and a bright orange outsole.',
+        image: '/images/products/offWhiteConverse.webp'
+        },
+        {
+        model:'Off White Converse',
+        description:'Virgil Abloh emphasizes accessibility and wearability with the OFF-WHITE x Chuck 70 White collab. As a result, the classic silhouette is ensconced in traditional cotton canvas and finished in a versatile crisp white shade. Virgil retains the shoes signature and rubber license plate, though the latter is turned upside down. Additional flourishes include the OFF-WHITEs trademark diagonal midsole stripes, printed text on the medial side, and a bright orange outsole.',
+        image: '/images/products/offWhiteConverse.webp'
+        },
+        {
+        model:'Off White Converse',
+        description:'Virgil Abloh emphasizes accessibility and wearability with the OFF-WHITE x Chuck 70 White collab. As a result, the classic silhouette is ensconced in traditional cotton canvas and finished in a versatile crisp white shade. Virgil retains the shoes signature and rubber license plate, though the latter is turned upside down. Additional flourishes include the OFF-WHITEs trademark diagonal midsole stripes, printed text on the medial side, and a bright orange outsole.',
+        image: '/images/products/offWhiteConverse.webp'
+        },
+        {
         model:'Nike Dunk',
-        description:'Zapatillas de alta calidad. Par indispensable e imperdible, únicas en el país, no perderse esta oportunidad',
+        description:'The Dunk Low Black White brings a classic two-tone look to its classic basketball construction. The shoes low-top build emerges in leather, with a white base contrasted by black overlays, with further black on the Swoosh branding. Perforations on the toe box provide breathability, while underfoot, the two-tone look is matched by the tooling, which incorporates a concentric rubber outsole for traction.',
         image: '/images/products/nikeDunk.png'
         },
-         {
+        {
         model:'Nike Dunk',
-        description:'Zapatillas de alta calidad. Par indispensable e imperdible, únicas en el país, no perderse esta oportunidad',
+        description:'The Dunk Low Black White brings a classic two-tone look to its classic basketball construction. The shoes low-top build emerges in leather, with a white base contrasted by black overlays, with further black on the Swoosh branding. Perforations on the toe box provide breathability, while underfoot, the two-tone look is matched by the tooling, which incorporates a concentric rubber outsole for traction.',
         image: '/images/products/nikeDunk.png'
         },
-         {
+        {
         model:'Nike Dunk',
-        description:'Zapatillas de alta calidad. Par indispensable e imperdible, únicas en el país, no perderse esta oportunidad',
+        description:'The Dunk Low Black White brings a classic two-tone look to its classic basketball construction. The shoes low-top build emerges in leather, with a white base contrasted by black overlays, with further black on the Swoosh branding. Perforations on the toe box provide breathability, while underfoot, the two-tone look is matched by the tooling, which incorporates a concentric rubber outsole for traction.',
         image: '/images/products/nikeDunk.png'
         },
-         {
+        {
+        model:'Nike Dunk',
+        description:'The Dunk Low Black White brings a classic two-tone look to its classic basketball construction. The shoes low-top build emerges in leather, with a white base contrasted by black overlays, with further black on the Swoosh branding. Perforations on the toe box provide breathability, while underfoot, the two-tone look is matched by the tooling, which incorporates a concentric rubber outsole for traction.',
+        image: '/images/products/nikeDunk.png'
+        },
+        {
         model:'Yeezy 350',
-        description:'Zapatillas de alta calidad. Par indispensable e imperdible, únicas en el país, no perderse esta oportunidad',
+        description:'Dropping with a reflective colorway, the Yeezy Boost 350 V2 Yecheil Non-Reflective emerges with a vibrant look. The upper is built with Primeknit, working a series of bright hues into its construction. Contrasted by a black monofilament stripe that runs through the design, a black TPU midsole underfoot houses a Boost unit, with a rubber outsole for traction.',
         image: '/images/products/yeezy350.png'
         },
-         {
+        {
         model:'Yeezy 350',
-        description:'Zapatillas de alta calidad. Par indispensable e imperdible, únicas en el país, no perderse esta oportunidad',
+        description:'Dropping with a reflective colorway, the Yeezy Boost 350 V2 Yecheil Non-Reflective emerges with a vibrant look. The upper is built with Primeknit, working a series of bright hues into its construction. Contrasted by a black monofilament stripe that runs through the design, a black TPU midsole underfoot houses a Boost unit, with a rubber outsole for traction.',
         image: '/images/products/yeezy350.png'
         },
-         {
+        {
         model:'Yeezy 350',
-        description:'Zapatillas de alta calidad. Par indispensable e imperdible, únicas en el país, no perderse esta oportunidad',
+        description:'Dropping with a reflective colorway, the Yeezy Boost 350 V2 Yecheil Non-Reflective emerges with a vibrant look. The upper is built with Primeknit, working a series of bright hues into its construction. Contrasted by a black monofilament stripe that runs through the design, a black TPU midsole underfoot houses a Boost unit, with a rubber outsole for traction.',
         image: '/images/products/yeezy350.png'
         },
-         {
+        {
         model:'Yeezy 350',
-        description:'Zapatillas de alta calidad. Par indispensable e imperdible, únicas en el país, no perderse esta oportunidad',
+        description:'Dropping with a reflective colorway, the Yeezy Boost 350 V2 Yecheil Non-Reflective emerges with a vibrant look. The upper is built with Primeknit, working a series of bright hues into its construction. Contrasted by a black monofilament stripe that runs through the design, a black TPU midsole underfoot houses a Boost unit, with a rubber outsole for traction.',
         image: '/images/products/yeezy350.png'
         },
         
