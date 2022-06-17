9fc97b23af6e869b9faf0df84897d82364ab9c7c
luis-cabello <lcabello@udesa.edu.ar>
2022-06-17 11:40:22 -0300

    validaciones de usuario logueado en comentarios, arreglos varios

--- myApp/controllers/productControllers.js
+++ myApp/controllers/productControllers.js
@@ -7,21 +7,21 @@ const comment = db.Comment
 const productController = {
     showProduct: (req, res) => {
         let id = req.params.id;
-        product.findByPk(id, 
-             { 
-                include: 
-                        { 
-                            all: true, 
-                            nested: true 
-                        } 
+        product.findByPk(id,
+            {
+                include:
+                {
+                    all: true,
+                    nested: true
+                }
             }
         )
-        .then((result) => {
+            .then((result) => {
 
-        return res.render('products', {product : result.dataValues})
-        }).catch((err) => {
-            console.log(err);
-        });
+                return res.render('products', { product: result.dataValues })
+            }).catch((err) => {
+                console.log(err);
+            });
     },
     showProductAdd: function (req, res) {
         return res.render('product-add', {
@@ -36,32 +36,32 @@ const productController = {
         let shoe = {
             photo: imgProduct,
             model: info.model,
-            description: info.description,   
-            users_id:  req.session.user.id
-        }; 
+            description: info.description,
+            users_id: req.session.user.id
+        };
 
 
         product.create(shoe)
-        .then((result) => {
-            return res.redirect('/')
-        }).catch((err) => {
-            return res.send('Hay un error' + err)
-        });
+            .then((result) => {
+                return res.redirect('/')
+            }).catch((err) => {
+                return res.send('Hay un error' + err)
+            });
 
     },
     showProductEdit: (req, res) => {
-    let id = req.params.id;
-    product.findByPk(id).then((result) => {
-    let shoe = {
-        id: result.id,
-        photo: result.photo,
-        model: result.model,
-        description: result.description,  
-    }
-    return res.render('product-edit', {product : shoe})
-    }).catch((err) => {
-        console.log(err);
-    });
+        let id = req.params.id;
+        product.findByPk(id).then((result) => {
+            let shoe = {
+                id: result.id,
+                photo: result.photo,
+                model: result.model,
+                description: result.description,
+            }
+            return res.render('product-edit', { product: shoe })
+        }).catch((err) => {
+            console.log(err);
+        });
     },
     updateProduct: (req, res) => {
         let info = req.body;
@@ -70,42 +70,47 @@ const productController = {
         let shoe = {
             photo: imgProduct,
             model: info.model,
-            description: info.description,  
+            description: info.description,
         }
 
         let filtro = {
-            where : {
-                id : req.params.id
+            where: {
+                id: req.params.id
             }
         };
         console.log(shoe);
-        
         product.update(shoe, filtro)
-        .then((result) => {
-            return res.redirect('/')
-        }).catch((err) => {
-            console.log(err);
-        });
+            .then((result) => {
+
+                return res.redirect('/')
+            }).catch((err) => {
+                console.log(err);
+            });
     },
-     deleteProduct: (req, res) => {
+    deleteProduct: (req, res) => {
         let id = req.params.id
         product.destroy({
-            where : { 
-                id : id
+            where: {
+                id: id
             }
         })
-        .then((result) => {
-            console.log(result);
-            return res.redirect('/')
-        })
+            .then((result) => {
+                console.log(result);
+                return res.redirect('/')
+            })
 
-     },
-    comments : (req, res) => {
+    },
+    comments: (req, res) => {
+        if (req.session.user == undefined) {
+            res.redirect('/profile/login')
+           
+        
+        } 
         let info = req.body
         let comentario = {
-            comentario : info.comentario,
+            comentario: info.comentario,
             products_id: req.params.id,
-            users_id : req.session.user.id,
+            users_id: req.session.user.id,
         }
         comment.create(comentario)
         .then((result) => {
@@ -113,8 +118,9 @@ const productController = {
         }).catch((err) => {
             console.log(err);
         });
+       
     },
-    
-} ;
+
+};
 
 module.exports = productController;
